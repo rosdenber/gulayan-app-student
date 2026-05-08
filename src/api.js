77b11e095b40677ce205bf6c6b757dbbe9c750da
rosdenber <rdpineda13@gmail.com>
2026-05-08 22:59:17 +0800

    Updated: feat: fetch plants data from server

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE_URL = "http://gulayan-server.test/api";
+const API_BASE_URL = "http://gulayan-server-student.test/api";
 
 export const api = axios.create({
    baseURL: API_BASE_URL,
